b9f6e6857ed4b0d9dbbcb27204ea061fc3e56a50
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-05-04 16:42:26 -0700

    Add gsap timeline animation

--- CookiePopup/app.js
+++ CookiePopup/app.js
@@ -1,3 +1,9 @@
 // gsap.to('.text',{y: 50, opacity: 0, duration: 1})
 
-gsap.fromTo('.text', {opacity: 0, y: 80}, {opacity: 1, y:0, duration: 1})
+// gsap.fromTo('.text', {opacity: 0, y: 80}, {opacity: 1, y:0, duration: 1})
+
+const tl = gsap.timeline({defaults:{duration: 0.75, ease: "power1.out",}})
+
+tl.fromTo('.cookie-container', {scale: 0}, {scale: 1})
+tl.fromTo('.cookie', {opacity: 0, x:-50, rotation: '-45deg'}, {opacity: 1, x:0, rotation: '0deg'})
+tl.fromTo('.text', {x:30, opacity:0}, {x: 0, opacity: 1}, '<')
