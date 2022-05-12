3d0fcace195fa7900c2a911b422a6f0a7610d4bb
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-05-11 18:09:10 -0700

    Add barba and gsap for page tranistions

---
+++ PageTransitions/app.js
@@ -0,0 +1,50 @@
+const tlLeave = gsap.timeline({
+  defaults: {duration: 0.75, ease: "Power2.easeOut"}
+});
+
+const tlEnter = gsap.timeline({
+  defaults: {duration: 0.75, ease: "Power2.easeOut"}
+});
+
+
+//Making functions for the leave and enter animations 
+
+const leaveAnimation = (current, done) =>{
+  const product = current.querySelector('.image-container');
+  const text = current.querySelector('.showcase-text');
+  const circles = current.querySelector('.circle');
+  const arrow = current.querySelector('.showcase-arrow');
+
+  return(
+    tlLeave.fromTo(arrow, {opacity:1, y:0}, {opacity: 0, y:50}),
+    tlLeave.fromTo(product, 
+      {y: 0, opacity: 1}, 
+      {y: 100, opacity: 0, onComplete: done}, 
+      '<'
+    ),
+    tlLeave.fromTo(text, {y: 0, opacity:1}, {opacity: 0, y: 100}, '<')
+    
+  )
+};
+
+//Run animations
+barba.init({
+  preventRunning: true,
+  transitions:[
+    //showcase transitions
+    {
+      name: "default",
+     
+      leave(data){
+        const done = this.async();
+        let current = data.current.container;
+        leaveAnimation(current, done);
+      },
+      enter(data){
+        const done = this.async();
+        let next = data.next.container;
+        gsap.fromTo(next, {opacity: 0}, {opacity:1, duration: 1, onComplete: done});
+      }
+    },
+  ],
+});
