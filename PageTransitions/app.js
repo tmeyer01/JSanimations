5ce6b875ddf1d752e798106d67b91f7ed90cad32
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-05-12 13:12:07 -0700

    add enter animation

--- PageTransitions/app.js
+++ PageTransitions/app.js
@@ -36,6 +36,48 @@ const leaveAnimation = (current, done) =>{
         y: -200,
         opacity: 0,
         stagger: 0.15,
+        ease: 'back.out(1.7)',
+        duration: 1,
+      },
+      '<'
+    )
+    
+  )
+};
+
+
+
+
+
+const enterAnimation = (current, done) =>{
+  const product = current.querySelector('.image-container');
+  const text = current.querySelector('.showcase-text');
+  const circles = current.querySelectorAll('.circle');
+  const arrow = current.querySelector('.showcase-arrow');
+
+  return(
+    tlLeave.fromTo(arrow, {opacity:0, y:50}, {opacity: 1, y:0}),
+    tlLeave.fromTo(
+      product, 
+      {y: -100, opacity: 0}, 
+      {y: 0, opacity: 1, onComplete: done}, 
+      '<'
+    ),
+    tlLeave.fromTo(
+      text, 
+      {y: 100, opacity:0}, 
+      {opacity: 1, y: 0}, 
+      '<'
+    ),
+    tlLeave.fromTo(
+      circles,
+      {y:-200, opacity: 0},
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.15,
+        ease: 'back.out(1.7)',
+        duration: 1,
       },
       '<'
     )
@@ -59,7 +101,8 @@ barba.init({
       enter(data){
         const done = this.async();
         let next = data.next.container;
-        gsap.fromTo(next, {opacity: 0}, {opacity:1, duration: 1, onComplete: done});
+        enterAnimation(next, done)
+        // gsap.fromTo(next, {opacity: 0}, {opacity:1, duration: 1, onComplete: done});
       }
     },
   ],
