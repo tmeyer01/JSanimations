fb393f3d4eac926c90e68d253037e550f1c3f8a6
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-05-12 13:37:05 -0700

    changing background gradenit on page change

--- PageTransitions/app.js
+++ PageTransitions/app.js
@@ -49,27 +49,28 @@ const leaveAnimation = (current, done) =>{
 
 
 
-const enterAnimation = (current, done) =>{
+const enterAnimation = (current, done, gradient) =>{
   const product = current.querySelector('.image-container');
   const text = current.querySelector('.showcase-text');
   const circles = current.querySelectorAll('.circle');
   const arrow = current.querySelector('.showcase-arrow');
 
   return(
-    tlLeave.fromTo(arrow, {opacity:0, y:50}, {opacity: 1, y:0}),
-    tlLeave.fromTo(
+    tlEnter.fromTo(arrow, {opacity:0, y:50}, {opacity: 1, y:0}),
+    tlEnter.to("body", { background: gradient }, "<"),
+    tlEnter.fromTo(
       product, 
       {y: -100, opacity: 0}, 
       {y: 0, opacity: 1, onComplete: done}, 
       '<'
     ),
-    tlLeave.fromTo(
+    tlEnter.fromTo(
       text, 
       {y: 100, opacity:0}, 
       {opacity: 1, y: 0}, 
       '<'
     ),
-    tlLeave.fromTo(
+    tlEnter.fromTo(
       circles,
       {y:-200, opacity: 0},
       {
@@ -92,18 +93,38 @@ barba.init({
     //showcase transitions
     {
       name: "default",
-     
+      once(data){
+        const done = this.async();
+        let next = data.next.container;
+        let gradient = getGradient(data.next.namespace);
+        gsap.set('body', {background: gradient});
+        enterAnimation(next,done,gradient)
+      },
+      enter(data){
+        const done = this.async();
+        let next = data.next.container;
+        let gradient = getGradient(data.next.namespace);
+        enterAnimation(next, done, gradient)
+        // gsap.fromTo(next, {opacity: 0}, {opacity:1, duration: 1, onComplete: done});
+      },
       leave(data){
         const done = this.async();
         let current = data.current.container;
         leaveAnimation(current, done);
       },
-      enter(data){
-        const done = this.async();
-        let next = data.next.container;
-        enterAnimation(next, done)
-        // gsap.fromTo(next, {opacity: 0}, {opacity:1, duration: 1, onComplete: done});
-      }
     },
   ],
 });
+
+
+//changing gradient on showcase 
+function getGradient(name) {
+  switch (name) {
+    case "handbag":
+      return "linear-gradient(260deg, #b75d62, #754d4f)";
+    case "boot":
+      return "linear-gradient(260deg, #5d8cb7, #4c4f70)";
+    case "hat":
+      return "linear-gradient(260deg, #b27a5c, #7f5450)";
+  }
+}
